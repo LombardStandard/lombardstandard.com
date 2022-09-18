8d7b73b9c1c5a3f276aab8a7af941f608507a7dc
AMG22075 <52190087+AMG22075@users.noreply.github.com>
2022-09-18 09:02:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function nFormatter(num) {
     { value: 1, symbol: '' },
     { value: 1e3, symbol: 'k' },
     { value: 1e6, symbol: 'M' },
-    { value: 1e9, symbol: 'G' },
+    { value: 1e9, symbol: 'B' },
     { value: 1e12, symbol: 'T' },
     { value: 1e15, symbol: 'P' },
     { value: 1e18, symbol: 'E' },
