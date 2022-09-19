d328b99f8e3ecfbd4fef3ed98ccf8e32b1b2efca
Suleman Tariq <suleman1220@gmail.com>
2022-09-19 08:22:23 +0500

    add i18n support and fix merge conflicts

--- js/script.js
+++ js/script.js
@@ -2,13 +2,6 @@ var d = new Date();
 var n = d.getFullYear();
 document.getElementById('copyright').innerHTML = n;
 
-window.onresize = closeMenuResize;
-
-function closeMenuResize() {
-  document.getElementById('mobileMenu').style.opacity = 0;
-  document.getElementById('mobileMenu').style.pointerEvents = 'none';
-}
-
 _linkedin_partner_id = '3365265';
 window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
 window._linkedin_data_partner_ids.push(_linkedin_partner_id);
@@ -104,5 +97,94 @@ function nFormatter(num) {
     regionPercentageDiv.innerHTML = '34%';
   }
 
-  regionTextDiv.innerHTML = `In your region (${regionMap[region]})`;
+  regionTextDiv.innerHTML = `(${regionMap[region] || 'APAC'})`;
 })();
+
+// i18n integration
+function updateContent() {
+  const i18nElements = document.getElementsByClassName('i18nelement');
+
+  for (const i18nElement of i18nElements) {
+    const key = i18nElement.getAttribute('data-i18n');
+    i18nElement.innerHTML = i18next.t(key);
+  }
+}
+
+function updateIframes() {
+  document.getElementById(
+    'iframe-sub'
+  ).src = `https://dev.lombardstandard.com/subscribeForm/?lang=${i18next.language}`;
+  document.getElementById(
+    'iframe-lead'
+  ).src = `https://dev.lombardstandard.com/search_leads_widget/?lang=${i18next.language}`;
+}
+
+async function i18Loader() {
+  const langs = ['en', 'ja', 'de'];
+  const langJsons = await Promise.all(
+    langs.map((lang) => fetch(`i18n/${lang}.json`).then((res) => res.json()))
+  );
+
+  const resources = langs.reduce((acc, lang, idx) => {
+    acc[lang] = { translation: langJsons[idx] };
+    return acc;
+  }, {});
+
+  await i18next.use(i18nextBrowserLanguageDetector).init({
+    fallbackLng: 'en',
+    debug: false,
+    resources,
+  });
+
+  updateContent();
+  updateIframes();
+
+  i18next.on('languageChanged', () => {
+    updateContent();
+    updateIframes();
+  });
+
+  const langSelector = document.getElementById('langSelector');
+  langSelector.removeAttribute('disabled');
+  langSelector.addEventListener('change', (e) => {
+    i18next.changeLanguage(e.target.value);
+  });
+  langSelector.value = i18next.language;
+
+  let closeText = 'OK';
+  let message = 'By viewing this website you agree to our';
+  let link = 'Privacy Policy';
+
+  switch (i18next.language) {
+    case 'ja':
+      closeText = 'わかった';
+      message = 'このウェブサイトを閲覧することにより、あなたは私たちの';
+      link = 'プライバシーポリシー';
+      break;
+    case 'de':
+      closeText = 'OK';
+      message = 'Durch das Betrachten dieser Website stimmen Sie unseren zu';
+      link = 'Datenschutz-Bestimmungen';
+      break;
+  }
+
+  const cookieScript = document.createElement('script');
+  cookieScript.src = 'https://cookieinfoscript.com/js/cookieinfo.min.js';
+  cookieScript.id = 'cookieinfo';
+  cookieScript.setAttribute('data-close-text', closeText);
+  cookieScript.setAttribute('data-font-size', '14px');
+  cookieScript.setAttribute('data-message', message);
+  cookieScript.setAttribute('data-link', '#111827');
+  cookieScript.setAttribute('data-linkmsg', `<b>${link}</b>.`);
+  cookieScript.setAttribute(
+    'data-moreinfo',
+    'https://lombardstandard.com/privacy-policy'
+  );
+  cookieScript.setAttribute('data-text-align', 'left');
+  cookieScript.setAttribute('data-bg', '#FDE68A');
+  cookieScript.setAttribute('data-cookie', 'Lombard Standard Analytics');
+
+  document.body.appendChild(cookieScript);
+}
+
+i18Loader();
