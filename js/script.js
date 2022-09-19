302857a0dfad8525cd6a7d0ba91115d88039ac39
AMG22075 <52190087+AMG22075@users.noreply.github.com>
2022-09-19 09:08:06 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,6 @@ var d = new Date();
 var n = d.getFullYear();
 document.getElementById('copyright').innerHTML = n;
 
-window.onresize = closeMenuResize;
-
-function closeMenuResize() {
-  document.getElementById('mobileMenu').style.opacity = 0;
-  document.getElementById('mobileMenu').style.pointerEvents = 'none';
-}
-
 _linkedin_partner_id = '3365265';
 window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
 window._linkedin_data_partner_ids.push(_linkedin_partner_id);
