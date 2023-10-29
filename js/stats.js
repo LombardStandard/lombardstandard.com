7fcd3c1797d80d0d511dd0b2948108f1953f4040
Alberto MG Saruggia <52190087+AMG22075@users.noreply.github.com>
2023-10-29 12:07:36 +0900

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -288,7 +288,6 @@ window.addEventListener('load', async () => {
         });
 
        document.getElementById('count-1').innerHTML = profile_count;
-       document.getElementById('count-2').innerHTML = profile_count;
        
        
         renderDealsMap('map-container', {
