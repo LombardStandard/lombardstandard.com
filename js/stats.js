eb0a70b9bebd91f794c2e7a8d49ba098cea23ea1
Suleman Tariq <suleman1220@gmail.com>
2024-02-07 15:43:35 +0500

    chore: add video on product page

--- js/stats.js
+++ js/stats.js
@@ -363,6 +363,18 @@ window.addEventListener('load', async () => {
     }
   }
 
+  function updateVideo() {
+    const video = document.getElementById('video')
+    const videoSrc = document.getElementById("videoSrc")
+  
+    if (video && videoSrc) {
+      const lang = i18next.language.includes('en') ? 'en' : i18next.language;
+      const videoURL = lang === 'ja' ? 'https://lombst.s3.amazonaws.com/website/AI+Matching+JA+(Captions).mp4' : 'https://lombst.s3.amazonaws.com/website/AI+Matching+(Captions).mp4'
+      videoSrc.setAttribute('src', videoURL);
+      video.load();
+    }
+  }
+
   async function i18Loader() {
     const langs = ['en', 'ja', 'de', 'zh-CN', 'zh-Hant', 'fr', 'it', 'es', 'pt'];
     const langJsons = await Promise.all(
@@ -386,6 +398,7 @@ window.addEventListener('load', async () => {
     updateContent();
     updateIframes();
     updateImages();
+    updateVideo();
 
     i18next.on('languageChanged', () => {
       // setDate();
@@ -393,6 +406,7 @@ window.addEventListener('load', async () => {
       updateContent();
       updateIframes();
       updateImages();
+      updateVideo();
     });
 
     const langSelector = document.getElementById('langSelector');
